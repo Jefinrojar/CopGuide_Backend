021f4dd8541224b32df785fa3a8f561f97f1c8eb
Jefin Rojar <jefinrojar@gmail.com>
2025-05-22 12:12:02 +0530

    url changes

--- chat.py
+++ chat.py
@@ -329,4 +329,4 @@ def health():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Use PORT env var if available
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
